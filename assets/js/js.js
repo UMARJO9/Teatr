54a972c243170daf0b4a9dc643d6eb5ef8cfaf0c
UMARJO9 <umarnurmadov12@gmail.com>
2024-11-26 10:37:44 +0500

    afisha

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,9 +1,6 @@
 function changeImage(newSrc) {
     const mainImage = document.getElementById('mainImage');
-
     mainImage.style.opacity = 0;
-
-
     setTimeout(() => {
         mainImage.src = newSrc;
         mainImage.style.opacity = 1;
